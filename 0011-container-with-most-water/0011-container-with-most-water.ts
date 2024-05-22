1d48d41ae76f1561452206afe5281079ad1e641b
Vitor Lucas Fujita Felício <92118734+vichelly@users.noreply.github.com>
2024-05-22 13:56:22 -0300

    Time: 80 ms (20.91%), Space: 59.5 MB (19.86%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.ts
@@ -0,0 +1,19 @@
+function maxArea(height: number[]): number {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        let currentArea = Math.min(height[left], height[right]) * (right - left);
+        
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+}
