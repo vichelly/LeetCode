33f02da8a250e7692386a2bfcf56deac13acac65
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-10 18:27:24 -0300

    att

--- 0001-two-sum-java/Solution.java
+++ 0001-two-sum-java/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
-            for(int k = 1; k < nums.length;k++){
+            for(int k = i + 1; k < nums.length;k++){
                 int sum = nums[i] + nums[k];
                 if(sum == target){
                     return new int[]{i,k};
