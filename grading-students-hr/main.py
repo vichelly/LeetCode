d9108acc640d2a71adac6b5646caf578a227cb9c
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-07-22 00:15:45 -0300

    comentarios

--- grading-students-hr/main.py
+++ grading-students-hr/main.py
@@ -12,3 +12,57 @@ def gradingStudents(grades):
     return correct_grades
 
 
+""" HackerLand University has the following grading policy:
+
+    Every student receives a 
+
+in the inclusive range from to
+.
+Any
+less than
+
+    is a failing grade.
+
+Sam is a professor at the university and likes to round each student's
+
+according to these rules:
+
+    If the difference between the 
+
+and the next multiple of is less than , round up to the next multiple of
+.
+If the value of
+is less than
+
+    , no rounding occurs as the result will still be a failing grade.
+
+Examples
+
+round to
+(85 - 84 is less than 3)
+do not round (result is less than 38)
+
+    do not round (60 - 57 is 3 or higher)
+
+Given the initial value of
+for each of Sam's
+
+students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function
+
+with the following parameter(s):
+
+    : the grades before rounding
+
+Returns
+
+    : the grades after rounding
+
+Input Format
+
+The first line contains a single integer,
+, the number of students.
+Each line of the subsequent lines contains a single integer,  """
